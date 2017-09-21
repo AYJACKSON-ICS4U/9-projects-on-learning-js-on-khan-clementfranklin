f30539629dfba8416867a7598ded798be5988c85
clementfranklin <31698115+clementfranklin@users.noreply.github.com>
2017-09-21 10:56:04 -0400

    Create Ad-Design.js
    this is a ad for khan academy

---
+++ Ad-Design.js
@@ -0,0 +1,12 @@
+fill(0, 219, 230);
+rect(0,0,600,400);
+fill(52, 158, 57);
+textSize(30);
+text("Hide a shrub!", 120, 30);
+textSize(30);
+text("Buy your shrub now 50% off!", 0, 87);
+text("Buy your shrub now 50% off!", 1, 89);
+
+image(getImage("avatars/mr-pants-green"), 160, 100, 60, 0);
+
+image(getImage("cute/TreeUgly"), 160, 100, 60, 0);
